a4cfde49bb0a831159acbf7efdd9ecf2f7519054
Zarir B. <zarirbhesania@gmail.com>
2015-02-05 11:13:14 +0530

    CommonJS exports - Crypt, Encrypt and Decrypt

--- AES.js
+++ AES.js
@@ -10,30 +10,21 @@ exports.Crypt = function(encrypt, text, iv, key, cipher, mode) {
 
 exports.Encrypt = function(encrypt, text, iv, key, cipher, mode) {
 	var block = mcrypt.get_block_size(cipher,mode);
-	Ti.API.info('Block Size : ' + block);
-	
+	console.log('Block Size : ' + block);
 	var pad = block - ((text.length) % block);
-	Ti.API.info('Pad : ' + pad);
-	
+	console.log('Pad : ' + pad);
 	var plainText = text + require('/Crypt/Helper').str_repeat(require('/Crypt/Helper').chr(pad), pad);
-	Ti.API.info('PlainText : ' + plainText);
-	
-    return mcrypt.Encrypt(plainText, key);
+	console.log('PlainText : ' + plainText);
+    	return mcrypt.Encrypt(plainText, key);
 };
 
 exports.Decrypt = function(decrypt, encrypted, iv, key, cipher, mode) {
-	
 	var decrypted = mcrypt.Decrypt(base64.decode(encrypted), key);
-	
 	var block = mcrypt.get_block_size(cipher,mode);
-	Ti.API.info('Block Size : ' + block);
-    
+	console.log('Block Size : ' + block);
 	var pad = require('/Crypt/Helper').ord(decrypted[(decrypted.length) - 1]);
-    
-    var len = decrypted.length;
-    
-    var pad = require('/Crypt/Helper').ord(decrypted[len-1]);
-    Ti.API.info('Dec Pad : ' + pad);
-    
-    return decrypted.substr(0,len-pad);
+    	var len = decrypted.length;
+    	var pad = require('/Crypt/Helper').ord(decrypted[len-1]);
+    	console.log('Dec Pad : ' + pad);
+    	return decrypted.substr(0,len-pad);
 };
